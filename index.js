bb2c00432614751dc2ff83691e6059302110b10c
jkeefe77 <jvkeefe@gmail.com>
2023-06-13 16:11:05 -0400

    update via task 2.5
    added various endpoints

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const express = require("express"),
+  app = express(),
   morgan = require("morgan"),
   fs = require("fs"), // import built in node modules fs and path
-  path = require("path");
+  path = require("path"),
+  bodyParser = require("body-parser"),
+  uuid = require("uuid");
 
-const app = express();
+app.use(bodyParser.json());
+
 // create a write stream (in append mode)
 // a ‘log.txt’ file is created in root directory
-
-let topMovies = [
+let users = [
   {
-    title: "The Thing",
+    id: 1,
+    name: "Kim",
+    favoriteMovies: [],
   },
   {
+    id: 2,
+    name: "Joe",
+    favoriteMovies: ["The Departed"],
+  },
+];
+
+let movies = [
+  {
+    title: "The Thing",
+    Genre: {
+      Name: "Horror",
+      Description:
+        "In film and television, horror is a category of narrative fiction (or semi-fiction) intended to strike fear with the viewer",
+    },
+    director: {
+      Name: "John Carpenter",
+    },
+
     title: "Jaws",
   },
   {
@@ -39,6 +62,93 @@ let topMovies = [
     title: "Unforgiven",
   },
 ];
+//Create new users
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("users need names");
+  }
+});
+
+//Update
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+//Create
+app.post("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favoriteMovies.push(movieTitle);
+    res.status(200).json(user);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+//Create
+app.post("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favoriteMovies.push(movieTitle);
+    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+//delete
+app.delete("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter(
+      (title) => title !== movieTitle
+    );
+    res
+      .status(200)
+      .send(`${movieTitle} has been removed from user ${id}'s array`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
+
+//delete
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    users = users.filter((user) => user.id != id);
+    res.status(200).send(`user ${id} has been deleted`);
+  } else {
+    res.status(400).send("no such user");
+  }
+});
 
 app.use(express.static("public/documentation.html"));
 // setup the logger
@@ -52,7 +162,41 @@ app.get("/documentation", (req, res) => {
 });
 
 app.get("/movies", (req, res) => {
-  res.json(topMovies);
+  res.status(200).json(movies);
+});
+
+app.get("/movies/:title", (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find((movie) => movie.title === title);
+  if (movie) {
+    return res.status(200).json(movie);
+  } else {
+    res.status(400).send("no such movie");
+  }
+});
+
+app.get("/movies/genre/:genreName", (req, res) => {
+  const { genreName } = req.params;
+  const genre = movies.find((movie) => movie.Genre.Name === genreName).Genre;
+
+  if (genre) {
+    return res.status(200).json(genre);
+  } else {
+    res.status(400).send("no such genre");
+  }
+});
+
+app.get("/movies/directors/:directorName", (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find(
+    (movie) => movie.director.Name === directorName
+  ).director;
+
+  if (director) {
+    return res.status(200).json(director);
+  } else {
+    res.status(400).send("no such director");
+  }
 });
 
 app.use((err, req, res, next) => {
