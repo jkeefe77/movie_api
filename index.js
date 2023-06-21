8f6b5f8a2f7fe3a17fb8e05c4efeabbcbd9299f4
jkeefe77 <jvkeefe@gmail.com>
2023-06-21 09:03:23 -0400

    index.js change
    changes to console.log (listening port)

--- index.js
+++ index.js
@@ -270,49 +270,7 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
-
-const http = require('http');
-http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-
-response.end('Hello Node!/n');
-}).listen(8080, () => {
+app.listen(8080, () => {
   console.log("Your app is listening on port 8080.");
 });
 
-// const http = require('http'),
-//   fs = require('fs'),
-//   url = require('url');
-
-// http.createServer((request, response) => {
-//   let addr = request.url,
-//     q = url.parse(addr, true),
-//     filePath = '';
-
-//   fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log('Added to log.');
-//     }
-//   });
-
-//   if (q.pathname.includes('documentation')) {
-//     filePath = (__dirname + '/documentation.html');
-//   } else {
-//     filePath = 'index.html';
-//   }
-
-//   fs.readFile(filePath, (err, data) => {
-//     if (err) {
-//       throw err;
-//     }
-
-//     response.writeHead(200, { 'Content-Type': 'text/html' });
-//     response.write(data);
-//     response.end();
-
-//   });
-
-// })app.listen(8080);
-// console.log('My test server is running on Port 8080.');
