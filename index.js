00e10a0cca8c7e7939d008ca3977b26de103792c
jkeefe77 <jvkeefe@gmail.com>
2023-06-30 15:26:12 -0400

    added heroku folder to gitignore
    heroku ignore

--- index.js
+++ index.js
@@ -93,8 +93,23 @@ app.post(
 //Update
 app.put(
   "/users/:Username",
+  [
+    check("Username", "Username is required").isLength({ min: 5 }),
+    check(
+      "Username",
+      "Username contains non alphanumeric characters - not allowed."
+    ).isAlphanumeric(),
+    check("Password", "Password is required").not().isEmpty(),
+    check("Email", "Email does not appear to be valid").isEmail(),
+  ],
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    let hashedPassword = Users.hashPassword(req.body.Password);
     Users.findOneAndUpdate(
       { Username: req.params.Username },
       {
