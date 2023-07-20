92e35fd2df19e0c9d4aae6ba551c71bd0315e1c2
jkeefe77 <jvkeefe@gmail.com>
2023-07-20 15:02:36 -0400

    auth.js
    added .js to ./auth and ./passport

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ const jwtSecret = "your_jwt_secret";
 const jwt = require("jsonwebtoken"),
   passport = require("passport");
 
-require("./passport");
+require("./passport.js");
 
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const Users = Models.User;
 const cors = require("cors");
 app.use(cors());
 
-let auth = require("./auth")(app);
+let auth = require("./auth.js")(app);
 const passport = require("passport");
 require("./passport.js");
 
