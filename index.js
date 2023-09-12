01dc787249df72472b4f1bb0227193bb147ff89e
jkeefe77 <jvkeefe@gmail.com>
2023-09-12 08:49:53 -0400

    CORS update

--- index.js
+++ index.js
@@ -23,29 +23,30 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 const cors = require("cors");
+app.use(cors());
 
-let allowedOrigins = [
-  "http://localhost:8080",
-  "http://localhost:1234",
-  "https://myflixcinema.netlify.app",
-  "https://localhost:4200",
-];
+// let allowedOrigins = [
+//   "http://localhost:8080",
+//   "http://localhost:1234",
+//   "https://myflixcinema.netlify.app",
+//   "https://localhost:4200",
+// ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         // If a specific origin isn’t found on the list of allowed origins
+//         let message =
+//           "The CORS policy for this application doesn’t allow access from origin " +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 let auth = require("./auth.js")(app);
 const passport = require("passport");
